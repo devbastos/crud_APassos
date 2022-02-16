8b15d474afa5c013459571ffb74733cc9f294ada
Gabriel Bastos <86385468+devbastos@users.noreply.github.com>
2022-02-16 17:03:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const closeModal = () => {
 }
 
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client'))
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
 const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient))
 
 // CRUD - create read update delete
